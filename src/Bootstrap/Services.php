a8374628e8f095f0f3f3eacba4d6fd62b8f01f7a
thomasjthomasj <thomasjmarchant@hotmail.com>
2015-02-23 16:48:49 +0000

    added service container

---
+++ src/Bootstrap/Services.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Message\Mothership\DiscountReward\Bootstrap;
+
+use Message\Mothership\DiscountReward;
+use Message\Cog\Bootstrap\ServicesInterface;
+
+class Services implements ServicesInterface
+{
+	public function registerServices($services)
+	{
+
+	}
+}
